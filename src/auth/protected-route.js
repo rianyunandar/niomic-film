2cd63aecdb6fdd0aaf5be513fb49032fd109166e
rianyunandar <rianyunandar@gmail.com>
2021-03-05 08:19:42 +0700

    update

--- src/auth/protected-route.js
+++ src/auth/protected-route.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
+import Loading from '../component/loading'
 
-    const loadingImg ="https://cdn.auth0.com/blog/auth0-react-sample/assets/loading.svg"
 
 const ProtectedRoute = ({ component, ...args }) => (
   <Route
     component={withAuthenticationRequired(component, {
-      onRedirecting: () =><div className="spinner">
-      <img src={loadingImg} alt="Loading..." />
-    </div>,
+      onRedirecting: () =>
+      <Loading />,
     })}
     {...args}
   />
